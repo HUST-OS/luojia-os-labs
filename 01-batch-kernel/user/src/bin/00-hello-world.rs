6b3ca2d13c5d15eebd7bd17eb92a3c2f70beaba0
luojia65 <me@luojia.cc>
2021-04-07 01:52:04 +0800

    Batch kernel user

---
+++ 01-batch-kernel/user/src/bin/00-hello-world.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+#[macro_use]
+extern crate batch_kernel_user;
+
+#[no_mangle]
+fn main() -> i32 {
+    println!("Hello, world!");
+    unsafe { asm!("sret") }; // illegal
+    0
+}
